98247a151f11c28a8ed0df76ae9923861043f755
Baran <abybaranyilmaz@gmail.com>
2019-07-29 16:12:54 +0300

    added rabbit consts

---
+++ data-collector/src/main/java/com/exastax/notebook/constants/RabbitConsts.java
@@ -0,0 +1,14 @@
+package com.exastax.notebook.constants;
+
+/**
+ * @author : Baran YILMAZ
+ * @since : 29.07.2019
+ **/
+public interface RabbitConsts {
+    public static String HOST = "35.242.253.97";
+    public static String USERNAME = "admin";
+    public static String PASSWORD = "admin";
+    public static String VIRTUALHOST = "/";
+    public static Integer PORT = 5672;
+
+}
